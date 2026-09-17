1048477a276af64a4517df17fae0114894f7e499
tusharswami003 <tusharswami7428@gmail.com>
2026-09-17 21:07:11 +0530

    2 DB instances deployed

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -14,8 +14,20 @@ output "security_group_id" {
 }
 
 output "app_servers" {
-  description = "Details of all dev EC2 instances"
+  description = "Details of all dev APP EC2 instances"
 
+  value = {
+    for name, server in module.ec2 :
+    name => {
+      instance_id = server.instance_id
+      private_ip  = server.private_ip
+    }
+  }
+}
+
+output "db_servers" {
+  description = "Details of all dev DB EC2 instances"
+  
   value = {
     for name, server in module.ec2 :
     name => {
