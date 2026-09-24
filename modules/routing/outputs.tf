cc61f245374dab853cc478745e2c0df47aa7eb95
tusharswami003 <tusharswami7428@gmail.com>
2026-09-24 13:29:23 +0530

    Added NAT module, app ec2 launch data to run ngninx and updated routing to use NAT

--- modules/routing/outputs.tf
+++ modules/routing/outputs.tf
@@ -1,8 +1,3 @@
-output "internet_gateway_id" {
-  description = "ID of the Internet Gateway"
-  value       = aws_internet_gateway.this.id
-}
-
 output "public_route_table_id" {
   description = "ID of the public route table"
   value       = aws_route_table.public.id
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "this" {
-  cidr_block = var.vpc_cidr
+  cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
 
